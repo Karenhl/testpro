f762119d7af4243be017dcd446121c0f1bba15da
karen <451390409@qq.com>
2021-07-13 14:08:24 +0800

    提交测试文件

--- exc_pytest/test_cal.py
+++ exc_pytest/test_cal.py
@@ -1,6 +1,7 @@
+# coding=utf-8
 import pytest
 
-from testpro.Calcutator import Calcutator
+from testpro.exc_pytest.Calcutator import Calcutator
 
 
 class TestCal(object):
@@ -11,15 +12,17 @@ class TestCal(object):
 
     @pytest.mark.parametrize('x,y,exceptnum', [
         (1, 1, 2),
-        (2, 2, 4)
-    ])
+        (-2, 2, 0)
+    ], ids=["正数相加", "负数相加"])
+    @pytest.mark.run(order=-1)
     def test_add(self, x, y, exceptnum):
         assert self.calc.add(x, y) == exceptnum
 
     @pytest.mark.parametrize('x,y,exceptnum', [
         (1, 1, 1),
-        (4, 2, 2)
-    ])
+        (4, -2, -2)
+    ], ids=["正数相除", "负数相除"])
+    @pytest.mark.run(order=1)
     def test_div(self, x, y, exceptnum):
         assert self.calc.div(x, y) == exceptnum
 
